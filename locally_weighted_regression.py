6512eaa97a2ab8172eea38233f58f464a8139256
sharathbp <32369302+sharathbp@users.noreply.github.com>
2019-12-22 23:18:23 +0530

    Updated file.

--- locally_weighted_regression.py
+++ locally_weighted_regression.py
@@ -1,11 +1,9 @@
-
 from math import ceil, pi
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def lowess(x, y, f=2./3., iter=3):
-
+    
     r = ceil(f*n)
     h = [np.sort(np.abs(x-xi))[r] for xi in x]
 
@@ -23,33 +21,23 @@ def lowess(x, y, f=2./3., iter=3):
                           [np.sum(weights*x), np.sum(weights*x*x)]])
             beta = np.linalg.solve(A, b)
             yest[i] = beta[0] + beta[1]*x[i]
-        
-        plt.figure(figsize=(7,5))
-        plt.plot(x, y, label='y noisy')
-        plt.plot(x, yest, label='y pred')
-        plt.title("Iteration " + str(j+1))
-        plt.legend()
-            
         residuals = y - yest
         mid = np.median(np.abs(residuals))
         delta = np.clip(residuals/(6.0*mid), -1, 1)
-        delta = (1-delta**2)**2
-        
+        delta = (1-delta**2)**2    
     return yest
 
 if __name__=='__main__':
     n = 100
     x = np.linspace(0, 2*pi, n)
-    #print("=================value of x ===================")
-    #print(x)
+    print("=================value of x ===================")
+    print(x)
     y = np.sin(x) + 0.3*np.random.randn(n)
-    y[25] = 4
-    y[40] = 3
-    y[75] = 4
-    #print("================vlaue of y ====================")
-    #print(y)
+    print("================vlaue of y ====================")
+    print(y)
     yest = lowess(x, y, f = 0.25, iter=3)
     
-    
-    
+    plt.plot(x, y, label='y noisy')
+    plt.plot(x, yest, label='y pred')
+    plt.legend()
     
